4344b2af9bb5bb6e8131120efc349de701d97655
Hernan415 <hernan.hernandezmendoza@students.dominican.edu>
2021-11-18 10:53:46 -0800

    Initial commit

---
+++ game.py
@@ -0,0 +1,31 @@
+#Game that has a path that changes with power ups and obstacles and you as a player have to make it to 100 points to win
+
+#path
+    #Width
+        #Modify as time goes on, no smaller than player + obstacles width
+
+#obstacles
+    # shape
+        #Random pixles between 1-20 x 1-20
+    # points
+        # Minus 1-10 depending on pixel size
+
+
+#player
+    # shape
+        # random pixels between 1-20 x 1-20
+    # Cordinates
+        # Arrow keys
+        # increase as more power ups are eaten
+
+#Power ups
+    # shape
+        # random pixels between 1-20 x 1-20
+    # points
+        # Add points when touched
+    #power up
+        #increase speed depending on color
+
+#Scoreboard
+    #points
+        # obstacles - power up
