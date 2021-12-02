f4aa26af4fc8c0915f8d735757ab442b7f7c97f6
Hernan415 <hernan.hernandezmendoza@students.dominican.edu>
2021-12-02 13:17:56 -0800

    Update Final.py

--- Final.py
+++ Final.py
@@ -12,26 +12,26 @@ class Budget():
         def __init__(self):
             self.hourlyPay = int(input("How much is your hourly pay: "))
             self.hoursWorked = int(input("How many hours do you plan on working this month? : "))
-            self.paycheck = self.hourlyPay * self.hoursWorked
+            self.paycheck = int(self.hourlyPay * self.hoursWorked)
 
     class Expenses():
         def __init__(self):
             self.bills = self.Bills()
             self.stocks = self.Stocks()
-            self.expenses = self.bills + self.stocks
+            self.expenses = int(self.bills + self.stocks)
 
         class Bills():
             def __init__(self):
                 self.food = int(input("How much money do you want to spend on food this month?: "))
                 self.rent = int(input("How much is your rent?: "))
                 self.utilities = int(input("How much is your utilities bill?: "))
-                self.bills = self.food + self.rent + self.utilities
+                self.bills = int(self.food + self.rent + self.utilities)
 
         class Stocks():
             def __init__(self):
                 self.dividends = int(input("If you get dividend payment from stocks, how much was this months?: "))
                 self.investments = int(input("How much money will you be investing in the stock market this month?: "))
-                self.outsideFinancials = self.dividends - self.investments
+                self.outsideFinancials = int(self.dividends - self.investments)
 
 
 def monthlyBudget():
