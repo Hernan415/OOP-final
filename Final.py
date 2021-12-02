e9b03f1e7dca2babddef47514fefed76b71e9daa
Hernan415 <hernan.hernandezmendoza@students.dominican.edu>
2021-12-01 22:19:55 -0800

    Update Final.py

--- Final.py
+++ Final.py
@@ -1,62 +1,30 @@
-#External Libraries
-import pygame
-import sys
-import random
+#Budgetting
 
-#Game screen
-SCREEN_WIDTH = 750
-SCREEN_HEIGHT = 750
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
 
-#Size
-OBJECT_SIZE = 20
 
-class Player():
+class Income():
     def __init__(self):
-        self.draw
-        self.position = (0,0)
-        self.color = 
+        self.hourlyPay = int(input("How much is your hourly pay: "))
+        self.hoursWorked = int(input("How many hours do you plan on working this month? : "))
+        self.paycheck = self.hourlyPay * self.hoursWorked
 
-    def draw(self,surface):
-        r = pygame.Rect((self.position[0], self.position[1]), (OBJECT_SIZE, OBJECT_SIZE))
-        pygame.draw.rect(surface, self.color, r)
-        pygame.draw.rect(surface, (93, 216, 228), r, 1)
+class Bills:
+    def __init__(self):
+        self.food = int(input("How much money do you want to spend on food this month?: "))
+        self.rent = int(input("How much is your rent?: "))
+        self.utilities = int(input("How much is your utilities bill?: "))
+        self.expenses = self.food + self.rent + self.utilities
 
-    def handle_keys(self):
-        for event in pygame.event.get():
-            if event.type == pygame.quit:
-                pygame.quit()
-                sys.exit()
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP or pygame.K_w:
-                    self.turn(UP)
-                elif event.key == pygame.K_DOWN or pygame.K_s:
-                    self.turn(DOWN)
-                elif event.key == pygame.K_LEFT or pygame.K_a:
-                    self.turn(LEFT)
-                elif event.key == pygame.K_RIGHT or pygame.K_d:
-                    self.turn(RIGHT)
-    def drawPath(surface):
-        for y in range(0, int(GRID_HEIGHT)):
-            for x in range(0, int(GRID_WIDTH)):
-                if (x + y) % 2 == 0: # for all even squares
-                    r = pygame.Rect((x * OBJECT_SIZE, y * OBJECT_SIZE), (OBJECT_SIZE, OBJECT_SIZE))
-                    pygame.draw.rect(surface,(93, 216, 228), r)
-                else: # for all odd squares
-                    rr = pygame.Rect((x * OBJECT_SIZE, y * OBJECT_SIZE), (OBJECT_SIZE, OBJECT_SIZE))
-                    pygame.draw.rect(surface, (84, 194, 205), rr)
-#Game
-def game():
-    pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
+#class Investments:
 
-    player = Player()
-    player.draw(screen)
+#class Percentage
 
-    while True:
-        player.handle_keys()
-        drawPath(screen)
+def budget():
+    income = Income()
+    bills = Bills()
+
+    #investments = Investments()
+    #percentage = Percentage
 
 
-
-game()
+budget()
