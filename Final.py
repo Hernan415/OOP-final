6af827c5831497680ac799fdaccf4091b518d327
Hernan415 <hernan.hernandezmendoza@students.dominican.edu>
2021-12-01 23:20:13 -0800

    Update Final.py

--- Final.py
+++ Final.py
@@ -1,37 +1,38 @@
 #Budgetting
 
 
-
-class Income():
+class Budget():
     def __init__(self):
-        self.hourlyPay = int(input("How much is your hourly pay: "))
-        self.hoursWorked = int(input("How many hours do you plan on working this month? : "))
-        self.paycheck = self.hourlyPay * self.hoursWorked
+        self.income = self.Income()
+        self.bills = self.Bills()
+        self.stocks = self.Stocks()
+        self.expenses = self.bills + self.stocks
+        self.emergancyFunds = self.income - self.expenses
+        print(self.emergancyFunds)
 
-class Bills:
-    def __init__(self):
-        self.food = int(input("How much money do you want to spend on food this month?: "))
-        self.rent = int(input("How much is your rent?: "))
-        self.utilities = int(input("How much is your utilities bill?: "))
-        self.expenses = self.food + self.rent + self.utilities
+    class Income():
+        def __init__(self):
+            self.hourlyPay = int(input("How much is your hourly pay: "))
+            self.hoursWorked = int(input("How many hours do you plan on working this month? : "))
+            self.paycheck = self.hourlyPay * self.hoursWorked
+            print(self.paycheck)
 
-class Stocks:
-    def __init__(self):
-        self.dividends = int(input("If you get dividend payment from stocks, how much was this months?: "))
-        self.investments = int(input("How much money will you be investing in the stock market this month?: "))
-        self.outsideFinancials = self.dividends - self.investments
+    class Bills():
+        def __init__(self):
+            self.food = int(input("How much money do you want to spend on food this month?: "))
+            self.rent = int(input("How much is your rent?: "))
+            self.utilities = int(input("How much is your utilities bill?: "))
+            self.bills = self.food + self.rent + self.utilities
+            print(self.bills)
 
-class Percentage:
-    def __init__(self):
-        
-
-
-
-def budget():
-    income = Income()
-    bills = Bills()
-    stocks = Stocks()
-    percentage = Percentage()
+    class Stocks():
+        def __init__(self):
+            self.dividends = int(input("If you get dividend payment from stocks, how much was this months?: "))
+            self.investments = int(input("How much money will you be investing in the stock market this month?: "))
+            self.outsideFinancials = self.dividends - self.investments
+            print(self.outsideFinancials)
 
 
-budget()
+def monthlyBudget():
+    budget = Budget()
+monthlyBudget()
