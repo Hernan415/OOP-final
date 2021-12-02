d3d61e560adf0f93c213e31843556530b906f4c3
Hernan415 <hernan.hernandezmendoza@students.dominican.edu>
2021-12-01 22:35:23 -0800

    Update Final.py

--- Final.py
+++ Final.py
@@ -15,16 +15,23 @@ class Bills:
         self.utilities = int(input("How much is your utilities bill?: "))
         self.expenses = self.food + self.rent + self.utilities
 
-#class Investments:
+class Stocks:
+    def __init__(self):
+        self.dividends = int(input("If you get dividend payment from stocks, how much was this months?: "))
+        self.investments = int(input("How much money will you be investing in the stock market this month?: "))
+        self.outsideFinancials = self.dividends - self.investments
 
-#class Percentage
+class Percentage:
+    def __init__(self):
+        
+
+
 
 def budget():
     income = Income()
     bills = Bills()
-
-    #investments = Investments()
-    #percentage = Percentage
+    stocks = Stocks()
+    percentage = Percentage()
 
 
 budget()
