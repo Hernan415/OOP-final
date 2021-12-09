90449f11872fa5f159fc6dc4380abc6a58b14d6a
Hernan415 <hernan.hernandezmendoza@students.dominican.edu>
2021-12-09 13:36:02 -0800

    Create final2.py

---
+++ final2.py
@@ -0,0 +1,28 @@
+#Budgetting calculator
+
+#Track investments
+    #Ticker and how many you will buy
+
+#Track mutliple budgets
+    #necesities
+        #rent
+        #food
+        #savings
+    #investments
+    #income
+
+#sources of income
+class sourceOfIncome():
+    def __init__(self):
+        self.trackSalary = []
+
+    #Track salaries
+    source = input(int("How many sources of income do you yave?"))
+    for
+
+budgets = []
+
+#run time code
+def runTime():
+
+runTime()
