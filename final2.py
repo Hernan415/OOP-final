4bb0acde35ff423ed47b5ae5405a72531dce6436
Hernan415 <hernan.hernandezmendoza@students.dominican.edu>
2021-12-09 13:56:27 -0800

    Update final2.py

--- final2.py
+++ final2.py
@@ -1,28 +1,84 @@
-#Budgetting calculator
+class B503020:
+    def necesities(self, a):
+        return a*.5
 
-#Track investments
-    #Ticker and how many you will buy
+    def wants(self, a):
+        return a*.3
 
-#Track mutliple budgets
-    #necesities
-        #rent
-        #food
-        #savings
-    #investments
-    #income
+    def savingsAndDebtRepayment(self, a):
+        return a*.2
 
-#sources of income
-class sourceOfIncome():
-    def __init__(self):
-        self.trackSalary = []
+class B702010:
+    def necesities(self, a):
+        return a*.7
 
-    #Track salaries
-    source = input(int("How many sources of income do you yave?"))
-    for
+    def wants(self, a):
+        return a*.2
 
-budgets = []
+    def savingsAndDebtRepayment(self, a):
+        return a*.1
 
-#run time code
-def runTime():
+#create a calculator object
+b503020 = B503020()
+b702010 = B702010()
 
-runTime()
+while True:
+
+    print("1: Would you like your budget in 503020")
+
+    print("2: Would you like your budget in 702010")
+
+    b = int(input("Please select which budget calculator you would like to use"))
+
+    if b == 1:
+        print("1: Necesities")
+        print("2: Wants")
+        print("3: Savings and Debt Repayment")
+
+        ch = int(input("Select financial statement: "))
+
+        #Make sure the user have entered the valid choice
+        if ch in (1, 2, 3):
+
+            #first check whether user want to exit
+            if(ch == 3):
+                break
+
+                #If not then ask fo the input and call appropiate methods
+                a = int(input("Please enter your income "))
+
+                if(ch == 1):
+                    print(b503020.necesities(a))
+                elif(ch == 2):
+                    print(b503020.wants(a))
+                elif(ch == 3):
+                    print(b503020.savingsAndDebtRepayment(a))
+
+        else:
+            print("Invalid Input")
+    elif b == 2:
+        print("1: Necesities")
+        print("2: Wants")
+        print("3: Savings and Debt Repayment")
+
+        ch = int(input("Select financial statement: "))
+
+        #Make sure the user have entered the valid choice
+        if ch in (1, 2, 3):
+
+            #first check whether user want to exit
+            if(ch == 3):
+                break
+
+                #If not then ask fo the input and call appropiate methods
+                a = int(input("Please enter your income "))
+
+                if(ch == 1):
+                    print(b702010.necesities(a))
+                elif(ch == 2):
+                    print(b702010.wants(a))
+                elif(ch == 3):
+                    print(b702010.savingsAndDebtRepayment(a))
+
+        else:
+            print("Invalid Input")
